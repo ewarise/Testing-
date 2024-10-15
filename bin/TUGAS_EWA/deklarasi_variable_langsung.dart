50851dc074c24977d8ca1bcaebbace7402544ec7
ewarise <ewarisepasifik04@gmail.com>
2024-10-15 10:18:26 +0800

    deklarasi variable

---
+++ bin/TUGAS_EWA/deklarasi_variable_langsung.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Deklarasi variabel dengan tipe data String
+  String name = 'Ewa Rise Pasifik';
+
+  // Mencetak nilai variabel name beberapa kali
+  print(name);
+  print(name);
+  print(name);
+  print(name);
+}
+
+
+// Kode Dart yang kamu kirimkan mendeklarasikan variabel name
+// dengan tipe data String dan mencetak nilai variabel tersebut empat kali. Berikut adalah penjelasan singkatnya:
